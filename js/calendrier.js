3c53d564a53aa456efda9516c53c4764a8962bdb
Guillaume <guillaume_scw@hotmail.com>
2019-10-07 15:22:51 -0400

    Continuation du calendrier

--- js/calendrier.js
+++ js/calendrier.js
@@ -1,20 +1,44 @@
 
 $(document).ready(function() {
   $( "#month" ).click(function() {
-    alert("Mois");
     var $this = $(this);
     calendar.view($this.data('calendar-view'));
   });
   $( "#day" ).click(function() {
-    alert( "Jour" );
     var $this = $(this);
     calendar.view($this.data('calendar-view'));
   });
 
-  $('.btn-group button[data-calendar-nav]').each(function() {
-		var $this = $(this);
-		$this.click(function() {
-			calendar.navigate($this.data('calendar-nav'));
-		});
-	});
+  $( "#next" ).click(function() {
+    var $this = $(this);
+    calendar.navigate($this.data('calendar-nav'));
+
+  });
+  $( "#prev" ).click(function() {
+    var $this = $(this);
+    calendar.navigate($this.data('calendar-nav'));
+  });
+
+
+
 });
+
+// Retourne si le courriel entré existe déjà dans la BD
+function evenement(){
+  alert("evenement");
+  var bool = true;
+  $.ajax({
+    type: "POST",
+    async: false,
+    url: "../../php/script/Horaire/ajouterHoraire.php",
+    data: {
+      test: "Test"
+  },
+    success: function(result){
+      alert("Sucess");
+    },
+    error: function (jQXHR, textStatus, errorThrown) {
+        alert("An error occurred whilst trying to contact the server: " + jQXHR.status + " " + textStatus + " " + errorThrown);
+    }
+  });
+}
---
+++ php/gestionnaire/Horaire/gestionHoraire.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* Gestionnaire pour les horaires
+* Contient les méthodes nécessaires
+* aux fonctionnements de l'horaire
+*
+* Nom :         GestionHoraire
+* Catégorie :   Classe
+* Auteur :      Guillaume Côté
+* Version :     1.0
+* Date de la dernière modification : 2019-10-07
+*/
+
+class GestionHoraire{
+
+
+  //Retourne vrai si le courriel en paramètre existe dans la BD
+  // public function courrielExisteDeja($courriel){
+  //   $conn = new Connexion();
+  //
+  //   $stmt = $conn->do()->prepare("SELECT fk_utilisateur
+  //     FROM compte_utilisateur
+  //     WHERE courriel = ?");
+  //     $stmt->bind_param('s', $courriel);
+  //
+  //     $stmt->execute();
+  //     $result = $stmt->get_result();
+  //     if($result->num_rows > 0){
+  //       $stmt->close();
+  //       return true;
+  //     }
+  //     else{
+  //       $stmt->close();
+  //       return false;
+  //     }
+  //
+  //     $stmt->close();
+  //   }
+
+
+
+
+
+ ?>
