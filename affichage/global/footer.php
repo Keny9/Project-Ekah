464e7a435fb5bc6424074505fad725531e8b6cd7
Karl <karlito1998@hotmail.com>
2019-10-07 19:33:37 -0400

    Création du modele footer

--- affichage/global/footer.php
+++ affichage/global/footer.php
@@ -1,5 +1,32 @@
 <footer>
   <div class="footer-main">
-
+    <div class="contact-link">
+      <a href="#">CONTACTEZ-NOUS</a>
+    </div>
+    <div class="block-photo">
+      <div class="photo">
+        <a href="#"></a>
+      </div>
+      <div class="photo">
+        <a href="#"></a>
+      </div>
+      <div class="photo">
+        <a href="#"></a>
+      </div>
+      <div class="photo">
+        <a href="#"></a>
+      </div>
+    </div>
+    <div class="block-media">
+      <div class="media-center">
+        <span class="media" id="instagram"></span>
+        <span class="media" id="facebook"></span>
+        <span class="media" id="spotify"></span>
+        <span class="media" id="email"><i class="far fa-envelope"></i></span>
+      </div>
+    </div>
+    <div class="ekah-txt">
+      <p>Collectif Ekah 2019</p>
+    </div>
   </div>
 </footer>
