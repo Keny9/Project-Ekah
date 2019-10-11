3f95b2fc102a7b54bd2edac72aaef9d32122681f
Karl <karlito1998@hotmail.com>
2019-10-10 20:34:48 -0400

    Footer + page reservation

--- affichage/client/reservation.php
+++ affichage/client/reservation.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" content="">
     <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" href="../../css/inscription.css">
     <link rel="stylesheet" href="../../css/reservation.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
     <script type="text/javascript" src="../../js/jquery-3.4.1.slim.js"></script>
@@ -16,13 +17,20 @@
     <title>Réservation</title>
   </head>
   <body>
-    <?php include "../global/header_client.php"; ?>
+    <?php //include "../global/header_client.php"; ?>
 
     <main>
-      <?php include "../global/side_menu.php"; ?>
-      <div class="content">
-
+      <div class="top-img">
+        <img src="../../img/mouvement_intuitif.png" alt="Mouvement Intuitif">
+        <div class="shade"></div>
+        <p class="txt-centered">Mouvement Intuitif</p>
       </div>
+      <div class="reservation">
+        <div class="txt-reservation txt-bienv">Réservez dès maintenant</div>
+      </div>
+
+
+
     </main>
 
     <?php include "../global/footer.php"; ?>
