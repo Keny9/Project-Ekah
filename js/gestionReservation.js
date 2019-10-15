021195d269276464c88321dcbc998945d584627e
Guldrion <willurk@videotron.ca>
2019-10-15 09:54:09 -0400

    Modifier 100%

--- js/gestionReservation.js
+++ js/gestionReservation.js
@@ -35,6 +35,45 @@
      divSelection.classList.add("selectionne");
    }
  }
+ function modifier(){
+   // Create our XMLHttpRequest object
+   var hr = new XMLHttpRequest();
+   // Create some variables we need to send to our PHP file
+   var url="../../php/script/Activite/modifierActivite.php";
+   for (i = 0; i <= 18; i++){
+     let divSelection = document.getElementById("Activite-"+i);
+     if(divSelection.classList.contains("selectionne")){
+       var id = i+1;
+     }
+   }
+   var nom = document.getElementById('nom').value;
+   var idType = document.getElementById('type').value;
+   console.log(idType);
+  var descriptionC = document.getElementById('descriptionC').value;
+  var descriptionL = "LONGUE";
+
+   $(function($) {
+       $.ajax({
+         url: url,
+         type:"POST",
+         async: false,
+         data: {id:id,nom: nom, idType:idType, descriptionC: descriptionC, descriptionL: descriptionL},
+         success: function(data) {
+           console.log(data);
+           if(!data){
+               alert("La modification s'est effectuée avec succès!");
+           }
+           else{
+             //  document.getElementById('erreurIdentifiant').innerHTML="L'identifiant existe déjà";
+               console.log(data);
+           }
+         } ,
+         error: function() {
+           alert('Error occured');
+         }
+       });
+     });
+ }
  function supprime(){
    // Create our XMLHttpRequest object
    var hr = new XMLHttpRequest();
