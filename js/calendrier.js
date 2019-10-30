96489d2625cbabce4e438252830cb242161fa91e
Guillaume <guillaume_scw@hotmail.com>
2019-10-30 11:09:23 -0400

    Enlever commentaires inutiles

--- js/calendrier.js
+++ js/calendrier.js
@@ -133,34 +133,3 @@ function getAllDispo(){
     }
   });
 }
-
-
-//Covertir les mois anglais en chiffre
-function convertirMois(mois){
-  switch(mois) {
-  case "January,":
-    return "01";
-  case "February,":
-    return "02";
-  case "March,":
-    return "03";
-  case "April,":
-    return "04";
-  case "May,":
-    return "05";
-  case "June,":
-    return "06";
-  case "July,":
-    return "07";
-  case "August,":
-    return "08";
-  case "September,":
-    return "09";
-  case "October,":
-    return "10";
-  case "November,":
-    return "11";
-  case "December,":
-    return "12";
-  }
-}
