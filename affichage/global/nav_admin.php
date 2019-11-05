0e996cc8ac0f7e4f2ab350ceb6f5f573ea6e80f5
Karl <karlito1998@hotmail.com>
2019-11-05 16:34:32 -0500

    Changement du menu pour admin compléter avec les bon onglet

--- affichage/global/nav_admin.php
+++ affichage/global/nav_admin.php
@@ -1,43 +1,48 @@
-<?php // TODO: delete le fichier? ?>
 <div class="side-menu">
   <ul class="list-menu">
     <li>
       <a class="link-menu" href="#">
         <span id="icon-compte" class="picto-menu"></span>
-        <span class="text-menu"><p>Gérer les spécialistes</p></span>
+        <span class="text-menu"><p>Gestion des spécialistes</p></span>
       </a>
     </li>
     <li>
-      <a class="link-menu" href="#">
+      <a class="link-menu" href="/Project-Ekah/affichage/gestionReservation/gestionReservation.php">
         <span id="icon-form" class="picto-menu"></span>
-        <span class="text-menu"><p>Gérer les rendez-vous</p></span>
+        <span class="text-menu"><p>Gestion des services</p></span>
       </a>
     </li>
     <hr class="hr-menu">
     <li>
-      <a class="link-menu" href="/Project-Ekah/affichage/gestionReservation/gestionReservation.php">
+      <a class="link-menu" href="/Project-Ekah/affichage/admin/gestion-client.php">
         <span id="icon-rendezvous" class="picto-menu"></span>
-        <span class="text-menu"><p>Gérer les activités</p></span>
+        <span class="text-menu"><p>Gestion des clients</p></span>
       </a>
     </li>
     <li>
       <a class="link-menu" href="#">
         <span id="icon-groupe" class="picto-menu"></span>
-        <span class="text-menu"><p>Gérer les groupes</p></span>
+        <span class="text-menu"><p>Gestion des groupes</p></span>
       </a>
     </li>
     <hr class="hr-menu">
     <li>
+      <a class="link-menu" href="/Project-Ekah/affichage/admin/consulter-reservation.php">
+        <span id="icon-book" class="picto-menu"></span>
+        <span class="text-menu"><p>Consulter les réservations</p></span>
+      </a>
+    </li>
+    <li>
       <a class="link-menu" href="#">
-        <span id="icon-user" class="picto-menu"></span>
-        <span class="text-menu"><p>Consulter un client</p></span>
+        <span id="icon-stat" class="picto-menu"></span>
+        <span class="text-menu"><p>Statistiques</p></span>
       </a>
     </li>
     <hr class="hr-menu">
     <li>
       <a class="link-menu" href="#">
         <span id="icon-direction" class="picto-menu"></span>
-        <span class="text-menu"><p>Statistsiques</p></span>
+        <span class="text-menu"><p>Se déconnecter</p></span>
       </a>
     </li>
   </ul>
