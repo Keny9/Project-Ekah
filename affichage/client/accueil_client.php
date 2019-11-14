f82fc80a9fc9affcf53240c97271ccd56fc40278
Guillaume <guillaume_scw@hotmail.com>
2019-11-14 12:02:37 -0500

    Le calendrier d'Atelier affiche les ateliers

--- affichage/client/accueil_client.php
+++ affichage/client/accueil_client.php
@@ -12,16 +12,86 @@ include $_SERVER['DOCUMENT_ROOT'].'/Project-Ekah/php/script/Login/connect.php';
     <meta charset="utf-8">
     <title>Accueil</title>
     <link rel="shortcut icon" href="../../img/favicon-ekah.ico" type="image/x-icon">
-    <script type="text/javascript" src="../../js/jquery-3.4.1.slim.js"></script>
+    <link rel="stylesheet" href="../../utils/bootstrap-calendar/components/bootstrap2/css/bootstrap.css">
+  	<link rel="stylesheet" href="../../utils/bootstrap-calendar/components/bootstrap2/css/bootstrap-responsive.css">
+  	<link rel="stylesheet" href="../../utils/bootstrap-calendar/css/calendar.css">
     <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" href="../../css/inscription.css">
+    <link rel="stylesheet" href="../../css/reservation.css">
+    <link rel="stylesheet" href="../../css/fix.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <script type="text/javascript" src="../../js/jquery-3.4.1.slim.js"></script>
+    <script type="text/javascript" src="../../utils/bootstrap-calendar/components/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="../../utils/bootstrap-calendar/components/underscore/underscore-min.js"></script>
+    <script type="text/javascript" src="../../utils/bootstrap-calendar/components/bootstrap2/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../../utils/bootstrap-calendar/components/jstimezonedetect/jstz.min.js"></script>
+
+    <script type="text/javascript" src="../../utils/bootstrap-calendar/js/calendar.js"></script>
+    <script type="text/javascript" src="../../utils/bootstrap-calendar/js/app.js"></script>
+
     <script type="text/javascript" src="../../js/global.js"></script>
+    <script type="text/javascript" src="../../js/ateliers.js"></script>
+
   </head>
   <body>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/Project-Ekah/affichage/global/header.php' ?>
 
     <main>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+      <div class="reservation">
+        <div class="">
+          <div class="page-header">
+            <h3 class=" h3"></h3>
+          </div>
+          <div class="btnsCalandrier">
+            <button type="button" class="bouton-re-que" name="button" id="prev"  data-calendar-nav="prev"><< Prev</button>
+            <button type="button" class="bouton-re-que" name="button" id="next"  data-calendar-nav="next">Next >></button>
+          </div>
+        </div>
+        <br><br>
+
+        <div id="calendar"></div>
+        <br><br>
+
+
+        <script type="text/javascript">
+
+        var calendar = $("#calendar").calendar(
+          {
+            tmpl_path: "../../utils/bootstrap-calendar/tmpls/",
+            weekbox: false,
+            events_source: "../../php/script/Horaire/AfficherAllAteliers.php",
+
+            onAfterViewLoad: function(view) {
+              $('.page-header h3').text(this.getTitle());
+            }
+          });
+        </script>
+
+<!-- MODAL -->
+        <!-- <div id="modal-modif-reservation" class="modal-modif-reservation">
+          <div class="modal-content">
+              <div class="modal-align-middle-mr">
+                 <label class="label-reservation" for="activite">Facilitateur</label>
+                 <div class="box-select">
+                   <select class="select-inscr input-long" name="service" id="service" onchange="changeListe(this);">
+                     <option class="option-vide" value="vide" selected="selected">Facilitateur</option>
+                     <?php
+                     foreach ($arrFacilitateur as $facilitateur){
+                       echo "<option value=\"".$facilitateur->getId()."\">".$facilitateur->getPrenom()." ".$facilitateur->getNom()."</option>";
+                     }
+                     ?>
+                   </select>
+                 </div>
+              </div>
+            <div class="modal-align-middle btn-modal-insc modal-align-middle-mr">
+              <button type="submit" class="btn-confirmer input-court btn-coller" name="button">Sauvegarder</button>
+              <button id="btn-annuler" type="button" class="btn-confirmer input-long btn-compte-existant btn-coller" name="button">Annuler</button>
+            </div>
+          </div>
+        </div> -->
+
+      </div>
     </main>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/Project-Ekah/affichage/global/footer.php' ?>
   </body>
--- js/ateliers.js
+++ js/ateliers.js
@@ -1,49 +1,24 @@
 /**
- * Page JS pour la reservation
+ * Page JS pour les ateliers
  *
- * Nom :         reservation.js
+ * Nom :         ateliers.js
  * Catégorie :   JavaScript
  * Auteur :      Guillaume Côté
  * Version :     1.0
  * Date de la dernière modification : 2019-11-14
  */
 
-var calendrier = null;
-
-
-
- //Fonction pour afficher les events du calendrier dans le calendrier
- function callAjax(){
-
-   var idFacilitateur = null;
-   idFacilitateur = $('.facilitateur-select').attr("id");
-   // idFacilitateur = 1;
-
-   if(idFacilitateur == null){
-     idFacilitateur = -1;
-   }
-
-   duree = $('#duree').val();
-
-
-   return $.ajax({
-     type: "POST",
-     async: false,
-     dataType: "json",
-     url: "../../php/script/Horaire/afficherAllEvents.php",
-     data: {idFacilitateur: idFacilitateur, duree: duree}
-   });
- }
+var calendar = null;
 
  //Les event on click pour les boutons
- function calendrierReady(calendrier){
+ function calendrierReady(){
    $('#0').toggleClass("selectionne");
 
    //Les boutons pour naviger dans le calendrier
    $( "#next" ).click(function() {
      $("#dispo").empty();
      var $this = $(this);
-     calendrier.navigate($this.data('calendar-nav'));
+     calendar.navigate($this.data('calendar-nav'));
      changerBackground();
      enleverDayView();
      selectionnerJour();
@@ -51,29 +26,11 @@ var calendrier = null;
    $( "#prev" ).click(function() {
      $("#dispo").empty();
      var $this = $(this);
-     calendrier.navigate($this.data('calendar-nav'));
+     calendar.navigate($this.data('calendar-nav'));
      changerBackground();
      enleverDayView();
      selectionnerJour();
    });
- }
-
- //Load le calendrier avec tous les events (De base)
- function loadCalendrier(events){
-   // console.log("Load");
-  calendrier = $("#calendar").calendar(
-     {
-       tmpl_path: "../../utils/bootstrap-calendar/tmpls/",
-       weekbox: false,
-       events_source: events,
-
-       onAfterViewLoad: function(view) {
-         $('.page-header h3').text(this.getTitle());
-       }
-     });
-     calendrierReady(calendrier);
-
-     return calendrier;
  }
 
  //Permet de sélectionner une date
@@ -85,13 +42,11 @@ var calendrier = null;
          // console.log("click");
          if($(this).hasClass("selectionne")){
            $(this).toggleClass("selectionne");
-           getAllDispo();
            $("#dispo").empty();
 
          }else{
            $('.selectionne').toggleClass("selectionne");
            $(this).toggleClass("selectionne");
-           getAllDispo();
 
            if($("#dispo").is(':empty')){
              $("#dispo").append($("<option></option>").val("-1").html("Aucune disponibilité"));
@@ -134,94 +89,11 @@ var calendrier = null;
      $todayTxt.css("font-size", "1.2em");
  }
 
- //Afficher les dispo du facilitateur choisi
- function getEvents(){
-   callAjax();
- }
-
-//Toujours mettre après getEvents
- function apresAjax(){
-   $.when(callAjax()).done(function(response){
-     calendrier = loadCalendrier(response.result);
-     calendrier.view();
-     return calendrier;
-   });
- }
-
- //get les dispo du jour selectionné pour les mettres dans un combobox
- function getAllDispo(){
-   var idFacilitateur = null;
-   var date = null;
-   var duree = null;
-
-   idFacilitateur = $('.facilitateur-select').attr("id");
-   // idFacilitateur = 1;
-
-   duree = $('#duree').val();
-
-   if(idFacilitateur == null){
-     idFacilitateur = -1;
-   }
-
-   // console.log($('.selectionne').children().data('calDate'));
-   date = $('.selectionne').children().data('calDate');
-
-   if(date == null){
-     date = "2000-01-01";
-   }
-
-   $.ajax({
-     type: "POST",
-     async: false,
-     dataType: "json",
-     url: "../../php/script/Horaire/afficherAllHoraireSelectionne.php",
-     data: {idFacilitateur: idFacilitateur,
-             date: date,
-             duree: duree
-          },
-     success: function(data){
-         // console.log(data);
-         //Puisque les dispo sont en Ms je vais devoir les convertir
-         $("#dispo").empty();
-
-         $.each(data.result, function (index) {
-           var time = data.result[index].date_debut;
-           var date = new Date(time);
-           // alert(date.toString());
-
-           var heure = date.getHours() + ":" + date.getMinutes();
-
-           if(date.getHours().toString().length == 1){
-             heure = "0" + heure;
-           }
-           if(date.getMinutes().toString().length == 1){
-             heure = heure + "0";
-           }
-
-           $("#dispo").append($("<option></option>").val(this['id']).html(heure));
-         });
-
-     },
-     error: function (jQXHR, textStatus, errorThrown) {
-         console.warn(jQXHR.responseText);
-         alert("An error occurred whilst trying to contact the server: " + jQXHR.status + " " + textStatus + " " + errorThrown);
-     }
-   });
- }
-
 
 //Page est chargé
 $(document).ready(function() {
-  apresAjax();
-
-  getEvents();
-
-  calendrier.view();
-
+  calendrierReady();
   changerBackground();
   enleverDayView();
   selectionnerJour();
-
-  choisirFacilitateur();
-
 });
