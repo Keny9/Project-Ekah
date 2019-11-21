b56d4c2e7b1902c7b1e5e3bab4a5e38f5760e803
Guldrion <willurk@videotron.ca>
2019-11-21 10:42:31 -0500

    ajout de table etat_activite, ajout de desactivation a la place de supprimer et correction de probleme avec la modification

--- php/script/Activite/modifierActivite.php
+++ php/script/Activite/modifierActivite.php
@@ -17,6 +17,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/Project-Ekah/php/gestionnaire/Activite/
 $ga = new GestionActivite();
 $activite = new Activite( $_POST['id'],
                         $_POST['idType'],
+                        $_POST['idEtat'],
                         $_POST['nom'],
                         $_POST['descriptionC'],
                         $_POST['descriptionL']);
