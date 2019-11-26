7b782a0b95959459acd479c7ed7981b9e88b4bf0
Maxime <maxime.lussier1996@live.ca>
2019-11-26 12:50:58 -0500

    Front end du formulaire de paiement (..)

--- affichage/client/paiement.php
+++ affichage/client/paiement.php
@@ -5,7 +5,6 @@ include $_SERVER['DOCUMENT_ROOT'].'/Project-Ekah/php/script/Login/connect.php';
 
 // Get les infos du client
 include $_SERVER['DOCUMENT_ROOT'].'/Project-Ekah/php/script/Client/getMonProfil.php';
-$me = json_decode($client_json, TRUE);
 
 $id_activite = $_POST['service'];
 $date_rendez_vous = $_GET['date_rendez_vous'];
@@ -35,26 +34,35 @@ $time = $dt->format('H:i');
 if ($no_adresse){
   $emplacement = $no_adresse." ".$rue.", ".$ville;
 }else $emplacement = "";
-// Pour les tests
-echo nl2br("$id_activite
-$date_rendez_vous
-$id_facilitateur
-$id_dispo
-$no_adresse
-$rue
-$ville
-$duree
-$prix
-$id_region
-");
 
-echo $me['prenom'];
+// Get les infos de la Réservation
+include $_SERVER['DOCUMENT_ROOT']."/Project-Ekah/php/script/Reservation/paiement-getInfoReservation.php";
+
+
+
+
+// Pour les tests
+// echo nl2br("$id_activite
+// $date_rendez_vous
+// $id_facilitateur
+// $id_dispo
+// $no_adresse
+// $rue
+// $ville
+// $duree
+// $prix
+// $id_region
+// ");
+
+
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <link rel="stylesheet" href="../../css/main.css">
     <link rel="stylesheet" href="../../css/paiement.css">
     <script type="text/javascript" src="../../js/jquery-3.4.1.slim.js"></script>
@@ -65,49 +73,57 @@ echo $me['prenom'];
     <script type="text/javascript" src="../../js/paiement.js"></script>
   </head>
   <body>
+
     <form action="/Project-Ekah/php/script/Reservation/redirectQuestionnaire.php?" method="post" id="payment-form">
-
-      <div class="info-client">
-        <label>Prénom :</label>
-        <span><?php echo $client['prenom'] ?></span>
-        <label>Nom :</label>
-        <span><?php echo $client['nom'] ?></span>
-        <label>Service :</label>
-        <span><?php echo "" ?></span>
-        <label>Date :</label>
-        <span><?php echo $date ?></span>
-        <label>Heure :</label>
-        <span><?php echo $time ?></span>
-        <label>Emplacement :</label>
-        <span><?php echo $emplacement ?></span>
-        <label>Avec :</label>
-        <span><?php echo "" ?></span>
-        <label>Montant :</label>
-        <span><?php echo "" ?></span>
+      <div class="paiement-form-header">
+        <p>Formulaire de paiement Ekah</p>
+        <p>Afin de sauvegarder la réservation, vous devez procéder au paiement.</p>
       </div>
 
 
-
-      <div class="form-row">
-
-        <br>
-        <label for="card-element">
-          Crédit ou débit
-        </label>
-        <div id="card-element">
-          <!-- A Stripe Element will be inserted here. -->
+      <div class="paiement-form-main">
+        <div class="info-client">
+          <label>Prénom :</label>
+          <span><?php echo $client['prenom'] ?></span>
+          <label>Nom :</label>
+          <span><?php echo $client['nom'] ?></span>
+          <label>Service :</label>
+          <span><?php echo $service_nom ?></span>
+          <label>Date :</label>
+          <span><?php echo $date ?></span>
+          <label>Heure :</label>
+          <span><?php echo $time ?></span>
+          <label>Durée :</label>
+          <span><?php echo $duree." minutes" ?></span>
+          <label>Emplacement :</label>
+          <span><?php echo $emplacement ?></span>
+          <label>Montant :</label>
+          <span><?php echo $prix." $ CAD" ?></span>
         </div>
-        <div id="prix">
-          <label><?php echo $prix ?></label>
-          <input type="hidden" name="total" id="total" value="<?php echo $prix ?>">
-        </div>
-
-        <!-- Used to display Element errors. -->
-        <div id="card-errors" role="alert"></div>
       </div>
 
-      <button>Soumettre paiement</button>
+      <div class="paiement-form-footer">
+        <div class="form-row">
+          <label for="card-element">
+            Crédit ou débit
+          </label>
+          <div id="card-element">
+            <!-- A Stripe Element will be inserted here. -->
+          </div>
+          <div id="prix">
+            <input type="hidden" name="total" id="total" value="<?php echo $prix ?>">
+          </div>
+          <!-- Used to display Element errors. -->
+          <div id="card-errors" role="alert"></div>
+        </div>
+        <button>Soumettre paiement</button>
+      </div>
     </form>
+
+    <br>
+    <br>
+    <br>
+
   </body>
 
   </html>
