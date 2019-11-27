403c6daf3e49ccd7de8ff76647da3209905aba05
Karl <karlito1998@hotmail.com>
2019-11-27 11:07:46 -0500

    Changement css + commencement password recovery

---
+++ affichage/global/password-reset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Page pour permettre a un utilisateur de récupérer son mot de passe
+*
+* Nom :         password-reset.php
+* Catégorie :   Page
+* Auteur :      Karl Boutin
+* Version :     1.0
+* Date de la dernière modification : 2019-11-18
+*/
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" content="">
+    <title>Mot de passe oublié ?</title>
+    <link rel="shortcut icon" href="../../img/favicon-ekah.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/inscription.css">
+    <link rel="stylesheet" href="../../css/main.css">
+  </head>
+  <body>
+
+  </body>
+</html>
