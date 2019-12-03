136d7c1db16821402e941bee878d94e074cf61b5
Karl <karlito1998@hotmail.com>
2019-12-03 14:55:23 -0500

    Ajout de la légende et des couleurs pour les réservations d'un client

--- affichage/client/consulter-reservation.php
+++ affichage/client/consulter-reservation.php
@@ -47,6 +47,18 @@ session_start();
       <div class="reservation">
         <div id="txtConsulter" class="txt-consulter">Jettez un petit coup d'oeil à vos réservations</div>
 
+        <div class="legende">
+          <div class="legende-carr">
+            <div id="carre1" class="carre"></div><span class="txt-legende">Réservation annulée</span>
+          </div>
+          <div class="legende-carr">
+            <div id="carre2" class="carre"></div><span class="txt-legende">Réservation complétée</span>
+          </div>
+          <div class="legende-carr">
+            <div id="carre4" class="carre"></div><span class="txt-legende">Réservation à venir</span>
+          </div>
+        </div>
+
         <div class="block-tbl">
           <table id="table_reservation" class="cell-border hover row-border">
             <thead>
