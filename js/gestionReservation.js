0d99569484c47a632d79a5d6e550fdd716f89549
Guldrion <willurk@videotron.ca>
2019-12-04 10:44:20 -0500

    Regler probleme regex avec ;

--- js/gestionReservation.js
+++ js/gestionReservation.js
@@ -709,7 +709,7 @@ function ajouterActive(){
     return false;
   }
   function verifieNom(e){
-    var nomRegex = /^[0-9a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.:’ '-]+$/;
+    var nomRegex = /^[0-9a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.:;’ '-]+$/;
     //console.log(nomRegex.test(e));
     return nomRegex.test(e);
   }
@@ -722,11 +722,11 @@ function ajouterActive(){
     return false;
   }
   if(!verifieNom(nom)){
-    console.log("mauvais nom");
+    console.log("Mauvais nom");
     return false;
   }
   if(!verifieNom(descriptionC)){
-    console.log("mauvais description");
+    console.log("Mauvaise description");
     return false;
   }
 
