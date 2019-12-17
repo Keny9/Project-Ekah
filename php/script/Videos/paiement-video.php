5104211bebf8b4e69d5a70a56657ca45af27e9a9
Karl <karlito1998@hotmail.com>
2019-12-17 11:09:51 -0500

    Ajout visuel

---
+++ php/script/Videos/paiement-video.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Paiement d'une vidéo
+*
+* Nom :         redirectQuestionnaire
+* Catégorie :   scriptPhp
+* Auteur :      Maxime Lussier
+* Version :     1.1
+* Date de la dernière modification : 2019-12-03
+*/
+
+
+
+
+
+
+
+
+
+?>
